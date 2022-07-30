d3b2766772006a1feb18319c8a26c41d13ebbf3a
Pawan <makermindlane@gmail.com>
2022-07-31 00:06:40 +0530

    Added update_hardware()

--- led-driver/src/led_driver.c
+++ led-driver/src/led_driver.c
@@ -6,6 +6,7 @@ enum { ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON };
  * PRIVATE FUNCTION PROTOTYPES
  ***************************************************************************************/
 static u16 num_to_bit(i32 num);
+static void update_hardware();
 
 /****************************************************************************************
  * GLOBAL PRIVATE VARIABLES
@@ -20,23 +21,23 @@ static u16 leds_image;
 void LedDriver_create(u16* address) {
     leds_address = address;
     leds_image = ALL_LEDS_OFF;
-    *leds_address = leds_image;
+    update_hardware();
 }
 void LedDriver_destroy() {}
 
 void LedDriver_turn_on(i32 led_number) {
     leds_image |= num_to_bit(led_number);
-    *leds_address = leds_image;
+    update_hardware();
 }
 
 void LedDriver_turn_on_all() {
     leds_image = ALL_LEDS_ON;
-    *leds_address = leds_image;
+    update_hardware();
 }
 
 void LedDriver_turn_off(i32 led_number) {
     leds_image &= ~num_to_bit(led_number);
-    *leds_address = leds_image;
+    update_hardware();
 }
 
 /****************************************************************************************
@@ -44,3 +45,5 @@ void LedDriver_turn_off(i32 led_number) {
  ***************************************************************************************/
 
 static u16 num_to_bit(i32 num) { return 1 << (num - 1); }
+
+static void update_hardware() { *leds_address = leds_image; }
