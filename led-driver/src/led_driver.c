c0b0fdb918ab469686196076f66ca6e5d69b2193
Pawan <makermindlane@gmail.com>
2022-08-03 15:20:15 +0530

    Added bounds check in turn_on/off functions

--- led-driver/src/led_driver.c
+++ led-driver/src/led_driver.c
@@ -26,6 +26,8 @@ void LedDriver_create(u16* address) {
 void LedDriver_destroy() {}
 
 void LedDriver_turn_on(i32 led_number) {
+    if (led_number <= 0 || led_number > 16) return;
+
     leds_image |= num_to_bit(led_number);
     update_hardware();
 }
@@ -36,6 +38,8 @@ void LedDriver_turn_on_all() {
 }
 
 void LedDriver_turn_off(i32 led_number) {
+    if (led_number <= 0 || led_number > 16) return;
+
     leds_image &= ~num_to_bit(led_number);
     update_hardware();
 }
