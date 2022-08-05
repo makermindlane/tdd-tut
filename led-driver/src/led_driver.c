ed5545d22de190c897795da2845f72bdfd28af9e
Pawan <makermindlane@gmail.com>
2022-08-06 01:03:45 +0530

    Added helper functions. is_led_out_of_bounds(), set_led_image_bit(), clear_led_image_bit()

--- led-driver/src/led_driver.c
+++ led-driver/src/led_driver.c
@@ -1,14 +1,20 @@
 #include "led_driver.h"
 
+#include <stdbool.h>
+
 #include "runtime_error.h"
 
 enum { ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON };
+enum { FIRST_LED = 1, LAST_LED = 16 };
 
 /****************************************************************************************
  * PRIVATE FUNCTION PROTOTYPES
  ***************************************************************************************/
 static u16 num_to_bit(i32 num);
 static void update_hardware();
+static bool is_led_out_of_bounds(i32 led_number);
+static void set_led_image_bit(i32 led_number);
+static void clear_led_image_bit(i32 leds_number);
 
 /****************************************************************************************
  * GLOBAL PRIVATE VARIABLES
@@ -28,12 +34,12 @@ void LedDriver_create(u16* address) {
 void LedDriver_destroy() {}
 
 void LedDriver_turn_on(i32 led_number) {
-    if (led_number <= 0 || led_number > 16) {
+    if (is_led_out_of_bounds(led_number)) {
         RUNTIME_ERROR("LED Driver: out-of-bounds LED", led_number);
         return;
     }
 
-    leds_image |= num_to_bit(led_number);
+    set_led_image_bit(led_number);
     update_hardware();
 }
 
@@ -43,12 +49,12 @@ void LedDriver_turn_on_all() {
 }
 
 void LedDriver_turn_off(i32 led_number) {
-    if (led_number <= 0 || led_number > 16) {
+    if (is_led_out_of_bounds(led_number)) {
         RUNTIME_ERROR("LED Driver: out-of-bounds LED", led_number);
         return;
     }
 
-    leds_image &= ~num_to_bit(led_number);
+    clear_led_image_bit(led_number);
     update_hardware();
 }
 
@@ -59,3 +65,17 @@ void LedDriver_turn_off(i32 led_number) {
 static u16 num_to_bit(i32 num) { return 1 << (num - 1); }
 
 static void update_hardware() { *leds_address = leds_image; }
+
+static bool is_led_out_of_bounds(i32 led_number) {
+    if (led_number < FIRST_LED || led_number > LAST_LED) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static void set_led_image_bit(i32 led_number) { leds_image |= num_to_bit(led_number); }
+
+static void clear_led_image_bit(i32 leds_number) {
+    leds_image &= ~num_to_bit(leds_number);
+}
