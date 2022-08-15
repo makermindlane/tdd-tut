35814950d107265a0b5e6bd8e7e69b3487900b33
Pawan <makermindlane@gmail.com>
2022-08-15 22:27:11 +0530

    Added time_service.h

---
+++ light-scheduler/include/time_service.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "data_type_defs.h"
+
+typedef enum Day {
+    DAY_UNKNOWN = -1,
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+} Day;
+
+typedef struct Time {
+    i32 minute_of_day;
+    Day day_of_week;
+} Time;
+
+void TimeService_create();
+void TimeService_get_time(Time *const self);
