7e6f579b9c7c2278efccec08044156c420d9b9f8
Pawan <makermindlane@gmail.com>
2022-08-16 22:42:16 +0530

    Added light_scheduler.h/.c

---
+++ light-scheduler/include/light_scheduler.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void LightScheduler_create();
+void LightScheduler_destroy();
+void LightScheduler_wakeup();
---
+++ light-scheduler/src/light_scheduler.c
@@ -0,0 +1,13 @@
+#include "light_scheduler.h"
+
+void LightScheduler_create() {
+
+}
+
+void LightScheduler_destroy() {
+
+}
+
+void LightScheduler_wakeup() {
+
+}
