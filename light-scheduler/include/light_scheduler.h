e340f87650d49fb2e2ceebffa6b20f91ddcc332f
Pawan <makermindlane@gmail.com>
2022-08-16 23:21:33 +0530

    Added LightScheduler_schedule_turn_on()

--- light-scheduler/include/light_scheduler.h
+++ light-scheduler/include/light_scheduler.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "data_type_defs.h"
+#include "time_service.h"
+
 void LightScheduler_create();
 void LightScheduler_destroy();
 void LightScheduler_wakeup();
+void LightScheduler_schedule_turn_on(i32 id, Day day, i32 minute_of_day);
--- light-scheduler/src/light_scheduler.c
+++ light-scheduler/src/light_scheduler.c
@@ -11,3 +11,7 @@ void LightScheduler_destroy() {
 void LightScheduler_wakeup() {
 
 }
+
+void LightScheduler_schedule_turn_on(i32 id, Day day, i32 minute_of_day) {
+
+}
